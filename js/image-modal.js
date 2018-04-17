a783dc83de822072a51e0cd570e9c0c04f064538
niksoley <niksoley@gmail.com>
2018-04-17 11:53:37 -0300

    Alterações Fernando 1

--- js/image-modal.js
+++ js/image-modal.js
@@ -14,3 +14,11 @@ function reply_click(clicked_id) {
 modalImg.onclick = function() {
   modal.style.display = "none";
 }
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,6 +1,6 @@
 $('a').click(function() {
   $('html, body').animate({
-    scrollTop: ($($(this).attr('href')).offset().top) - 50
+    scrollTop: ($($(this).attr('href')).offset().top) - 0
   }, 500);
   return false;
 });
