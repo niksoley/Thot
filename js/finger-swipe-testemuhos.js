9a62a3b61fd7bc9dcac9527169995b0be4d98c5b
niksoley <niksoley@gmail.com>
2018-04-18 22:04:49 -0300

    Alteracoes Fernando 3

--- js/finger-swipe-testemuhos.js
+++ js/finger-swipe-testemuhos.js
@@ -1,16 +1,16 @@
-// finger swipe
+//finger swipe
 
-// $(".carousel").on("touchstart", function(event) {
-//   var xClick = event.originalEvent.touches[0].pageX;
-//   $(this).one("touchmove", function(event) {
-//     var xMove = event.originalEvent.touches[0].pageX;
-//     if (Math.floor(xClick - xMove) > 5) {
-//       $(this).carousel('next');
-//     } else if (Math.floor(xClick - xMove) < -5) {
-//       $(this).carousel('prev');
-//     }
-//   });
-//   $(".carousel").on("touchend", function() {
-//     $(this).off("touchmove");
-//   });
-// });
+$(".carousel").on("touchstart", function(event) {
+  var xClick = event.originalEvent.touches[0].pageX;
+  $(this).one("touchmove", function(event) {
+    var xMove = event.originalEvent.touches[0].pageX;
+    if (Math.floor(xClick - xMove) > 3) {
+      $(this).carousel('next');
+    } else if (Math.floor(xClick - xMove) < -3) {
+      $(this).carousel('prev');
+    }
+  });
+  $(".carousel").on("touchend", function() {
+    $(this).off("touchmove");
+  });
+});
--- js/image-modal.js
+++ js/image-modal.js
@@ -5,14 +5,25 @@ var modal = document.getElementById('myModal');
 var modalImg = document.getElementById("imgModal");
 
 function reply_click(clicked_id) {
-  modal.style.display = "block";
-  modalImg.src = document.getElementById(clicked_id).src;
-}
+  modal.style.display = "flex";
+  $("body").addClass("modal-open"); //block body scroll
 
+
+  var srcString = String(document.getElementById(clicked_id).src);
+
+  String.prototype.splice = function(idx, rem, str) {
+    return this.slice(0, idx) + str + this.slice(idx + Math.abs(rem));
+  };
+  var result = srcString.splice((srcString.length - 4), 0, "full");
+
+  modalImg.src = result;
+
+}
 
 // When the user clicks on modalImg, close the modal
 modalImg.onclick = function() {
   modal.style.display = "none";
+  $("body").removeClass("modal-open"); //remove blocked scroll from body
 }
 
 // Get the <span> element that closes the modal
@@ -21,4 +32,43 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
-}
+  $("body").removeClass("modal-open"); //remove blocked scroll from body
+};
+
+(function() {
+  var _overlay = document.getElementById('myModal');
+  var _clientY = null; // remember Y position on touch start
+
+  _overlay.addEventListener('touchstart', function(event) {
+    if (event.targetTouches.length === 1) {
+      // detect single touch
+      _clientY = event.targetTouches[0].clientY;
+    }
+  }, false);
+
+  _overlay.addEventListener('touchmove', function(event) {
+    if (event.targetTouches.length === 1) {
+      // detect single touch
+      disableRubberBand(event);
+    }
+  }, false);
+
+  function disableRubberBand(event) {
+    var clientY = event.targetTouches[0].clientY - _clientY;
+
+    if (_overlay.scrollTop === 0 && clientY > 0) {
+      // element is at the top of its scroll
+      event.preventDefault();
+    }
+
+    if (isOverlayTotallyScrolled() && clientY < 0) {
+      //element is at the top of its scroll
+      event.preventDefault();
+    }
+  }
+
+  function isOverlayTotallyScrolled() {
+    // https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollHeight#Problems_and_solutions
+    return _overlay.scrollHeight - _overlay.scrollTop <= _overlay.clientHeight;
+  }
+}())
