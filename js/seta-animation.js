1396a37bba32587f4b5db322fb95b6647e6a4a87
niksoley <niksoley@gmail.com>
2018-08-10 20:00:42 -0300

    setas

--- js/seta-animation.js
+++
@@ -1,8 +0,0 @@
-// $('#setaVoltar').on('mouseenter mouseleave', function(e) {
-//   $('#setasContainer').trigger(e.type);
-// })
-
-
-$('#setasContainer').click(function() {
-  $('#setaVoltar').trigger('click')
-})
